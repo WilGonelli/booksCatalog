387c95f990d74a365dbe9db3aaec0c6ec5e646bd
William <william.vieira@fit-tecnologia.org.br>
2025-06-24 12:12:48 -0300

    implemented homeScreen static

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -10,25 +10,42 @@ export default function HomePage() {
   const fetchBooks = async () => {
     const response = await getData();
     setBooks(response);
+    console.log(response);
   };
   useEffect(() => {
     fetchBooks();
   }, []);
   return (
     <main className={styles.containerHome}>
+      <div className={styles.containerHeader}>
+        <div className={styles.header}>
+          <p className={styles.bookWorld}>livros</p>
+          <button className={styles.btn}>novo</button>
+        </div>
+        <div className={styles.containerInput}>
+          <p>buscar</p>
+          <IoIosSearch className={styles.searchIcon} />
+        </div>
+      </div>
       {books.length > 0 && (
-        <>
-          <div className={styles.containerTitle}>
-            <div className={styles.title}>
-              <p>livros</p>
-              <p>botao</p>
-            </div>
-            <div className={styles.containerInput}>
-              <p>buscar</p>
-              <IoIosSearch className={styles.searchIcon} />
-            </div>
-          </div>
-        </>
+        <div className={styles.containerBooks}>
+          {books.map((book) => {
+            return (
+              <div key={book.id} className={styles.containerBook}>
+                <div className={styles.containerImage}>
+                  <img
+                    className={styles.image}
+                    src={`http://localhost:8080${book.image}`}
+                  />
+                </div>
+                <div className={styles.contaonerInfo}>
+                  <p className={styles.title}>{book.title}:</p>
+                  <p className={styles.description}>{book.description}:</p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
       )}
     </main>
   );
